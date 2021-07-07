d3f89fe50eebf70ba122ea51ba2c89fa034e4dbd
eyesunbidari-dev <eyesunbidari@gmail.com>
2021-07-07 20:24:23 +0430

    First commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import ToDoList from "./components/ToDoList";
+import ToDoNav from "./components/ToDoNav";
+
+
+class App extends Component {
+   
+    myMessage = ()=> {
+     prompt ( "عنوان تسک را مشخص کنید")
+     prompt ( " تاریخ را مشخص کنید")
+    };
+    render (){
+
+        return (
+          <div >
+              <ToDoNav/>
+             
+              <br/>
+              <button  className="btn float-right fa fa-check-square-o" onClick={this.myMessage}> کلیک کنید</button>
+              <br/>
+              <hr/>
+              <ToDoList/>
+              
+          </div>
+        )
+    }
+}
+ export default App;
